078acdbe30cfa3a10bcf7898eb55737649c1d2fa
xerxes-on <122219058+xerxes-on@users.noreply.github.com>
2025-02-25 23:23:06 +0500

    mainly done

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -20,27 +20,4 @@ class Branch extends Model
     {
         return $this->hasMany(Shipment::class, 'branch_id', 'id');
     }
-    public function sendMessage(int $conversationId, string $messageText, ?int $replyToMessageId = null): Message|JsonResponse
-    {
-        $user = Auth::user();
-
-        $conversation = Conversation::findOrFail($conversationId);
-
-        if (!$conversation->members()->where('user_id', $user->id)->exists()) {
-            return $this->commonHelper->returnResponse("Siz ushbu suhbatning aʼzosi emassiz", [], ResponseAlias::HTTP_FORBIDDEN);
-        }
-
-        $message = Message::create([
-            'conversation_id' => $conversation->id,
-            'sender_id' => $user->id,
-            'message' => $messageText,
-            'message_type' => ChatMessageType::TEXT,
-            'reply_to_message_id' => $replyToMessageId,
-        ]);
-        Log::info("Event Dispatching: MessageSent", ['message' => $message]);
-
-        broadcast(new MessageSent($message));
-        MessageSent::dispatch($message);
-        return $message;
-    }
 }
